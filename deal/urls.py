1588170e91a0ee33bc0619d4ff0b2b504dafc54e
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2025-11-28 12:25:56 +0200

    Added sidebar and django-debug-toolbar

--- deal/urls.py
+++ deal/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from deal.views import (
     index,
+    TagsListView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags/", TagsListView.as_view(), name="tags"),
 ]
 
 app_name = "deal"
--- deal/views.py
+++ deal/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from .models import Tag
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "deal/index.html")
+
+
+class TagsListView(generic.ListView):
+    model = Tag
+    queryset = Tag.objects.all()
