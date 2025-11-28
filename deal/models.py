ebe334f1b1baabcbc2d70d8a9047b59af2abd07c
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2025-11-28 11:54:47 +0200

    Develop models and migrate

--- deal/models.py
+++ deal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create model here
+class Tag(models.Model):
+    name = models.CharField(max_length=120)
+
+class Task(models.Model):
+    content = models.CharField(max_length=512)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    boolean_field = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
