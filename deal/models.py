fcd7ca2e886919337909e7df777386084cfd12c8
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2025-11-28 12:29:17 +0200

    Added migration to model Task with deadline blank and null = True

--- deal/models.py
+++ deal/models.py
@@ -6,6 +6,6 @@ class Tag(models.Model):
 class Task(models.Model):
     content = models.CharField(max_length=512)
     datetime = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField()
+    deadline = models.DateTimeField(null=True, blank=True)
     boolean_field = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag)
